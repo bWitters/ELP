d41c4ce679a8b430ea9fc42d6b8b10352be855a1
bWitters <witters.benjamin.2004@gmail.com>
2025-01-24 10:40:28 +0100

    Création de l'html, et modif du main

--- elm_proj/Projet/src/Main.elm
+++ elm_proj/Projet/src/Main.elm
@@ -25,14 +25,12 @@ main =
 
 type alias Model =
   { name : String
-  , password : String
-  , passwordAgain : String
   }
 
 
 init : Model
 init =
-  Model "" "" ""
+  Model ""
 
 
 
@@ -41,8 +39,6 @@ init =
 
 type Msg
   = Name String
-  | Password String
-  | PasswordAgain String
 
 
 update : Msg -> Model -> Model
@@ -50,12 +46,6 @@ update msg model =
   case msg of
     Name name ->
       { model | name = name }
-
-    Password password ->
-      { model | password = password }
-
-    PasswordAgain password ->
-      { model | passwordAgain = password }
 
 
 
@@ -66,20 +56,9 @@ view : Model -> Html Msg
 view model =
   div []
     [ viewInput "text" "Name" model.name Name
-    , viewInput "password" "Password" model.password Password
-    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
-    , viewValidation model
     ]
 
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
 viewInput t p v toMsg =
   input [ type_ t, placeholder p, value v, onInput toMsg ] []
-
-
-viewValidation : Model -> Html msg
-viewValidation model =
-  if model.password == model.passwordAgain then
-    div [ style "color" "green" ] [ text "OK" ]
-  else
-    div [ style "color" "red" ] [ text "Passwords do not match!" ]
